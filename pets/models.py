0ac5c70f4d2c35ab9869439c3146ebcdd4399f94
inv0w <jonjoninfante@gmail.com>
2020-05-07 18:56:48 -0700

    Navbar

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Musician(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+
+class Album(models.Model):
+    name = models.CharField(max_length=100)
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    genre = models.CharField(max_length=50)
+    publish_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+      return self.name
