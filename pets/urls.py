ef1c29467bf43ef8fce99e6dfb37a01d5e760cfe
inv0w <jonjoninfante@gmail.com>
2020-05-07 22:18:17 -0700

    Url pathing

--- pets/urls.py
+++ pets/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('pets/', views.pets, name="pets"),
+    path('pet/<int:pet_id>', views.pet, name="pet"),
+    path('calender/', views.calender, name="calender")
 ]
--- pets/views.py
+++ pets/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pet, Appointment
 
 
 def home(request):
     return render(request, 'home.html')
 
-# Create your views here.
+def pets(request):
+  context = {
+    'pets': Pet.objects.filter()
+  }
+  return render(request, 'pets.html', context)
+
+def pet(request, pet_id):
+    context = {
+    'pet': Pet.objects.get(id=pet_id),
+    'appointments' : Appointment.objects.all()
+    }
+    return render(request, 'pet.html', context)
+
+def calender(request):
+  context = {
+    'appointments': Appointment.objects.order_by('date_of_appointment')
+  }
+  return render(request, 'calender.html', context)
